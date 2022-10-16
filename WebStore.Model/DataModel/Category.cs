852a4bb5eb559e21952fbb1deb6dfdc7cba13dab
Antoni_Kijjaroentorn <ant@aha.com>
2022-10-16 17:16:38 +0200

    chore: fix class

--- WebStore.Model/DataModel/Category.cs
+++ WebStore.Model/DataModel/Category.cs
@@ -3,5 +3,5 @@ namespace WebStore.Model.DataModel;
 public class Category
 {
     public string CategoryType { get; set; } = default!;
-    public string CategoryName { get; set; }
+    public string CategoryName { get; set; } = default!;
 }
--- WebStore.Model/DataModel/Product.cs
+++ WebStore.Model/DataModel/Product.cs
@@ -5,12 +5,12 @@ namespace WebStore.Model.DataModel
     {
         public Category Category { get; set; } = default!;
         public string Description { get; set; } = default!;
-        public string Id { get; set; } = default!;
-        public string ImageBytes { get; set; } = default!;
+        public int Id { get; set; } = default!;
+        public byte[] ImageBytes { get; set; } = default!;
         public string Name { get; set; } = default!;
-        public string Price { get; set; } = default!;
-        public string Supplier { get; set; } = default!;
-        public string Weight { get; set; } = default!;
+        public decimal Price { get; set; } = default!;
+        public Supplier Supplier { get; set; } = default!;
+        public float Weight { get; set; } = default!;
 
     }
 }

b8d562a2dd1f9be4a8e18d79947b98dab9a99011
Rafal Grycuk <rafal@smth.smth>
2022-10-17 19:06:34 +0200

    Problems solved.

--- WebStore.DAL/EF/ApplicationDbContext.cs
+++ WebStore.DAL/EF/ApplicationDbContext.cs
@@ -1,47 +1,37 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using WebStore.Model.DataModels;
 
-namespace WebStore.DAL.EF
-{
-public class ApplicationDbContext : IdentityDbContext<User,Role,int>
-{
-    public virtual DbSet<Order> Orders {get; set; }
-    public virtual DbSet<OrderProduct> OrderProducts {get; set; }
-    public virtual DbSet<Product> Products {get; set; }
-    public virtual DbSet<ProductStock> ProductStocks {get; set; }
-    public virtual DbSet<Order> Orders {get; set; }
-    public virtual DbSet<Address> Addresses {get; set; }
-    public virtual DbSet<Category> Categories {get; set; }
-    public virtual DbSet<Invoice> Invoices {get; set; }
-    public virtual DbSet<StationaryStore> StationaryStores {get; set; }
+namespace WebStore.DAL.EF {
+    public class ApplicationDbContext : IdentityDbContext<User, IdentityRole<int>, int> {
+        public virtual DbSet<OrderProduct> OrderProducts { get; set; } = default!;
+        public virtual DbSet<Product> Products { get; set; } = default!;
+        public virtual DbSet<ProductStock> ProductStocks { get; set; } = default!;
+        public virtual DbSet<Order> Orders { get; set; } = default!;
+        public virtual DbSet<Address> Addresses { get; set; } = default!;
+        public virtual DbSet<Category> Categories { get; set; } = default!;
+        public virtual DbSet<Invoice> Invoices { get; set; } = default!;
+        public virtual DbSet<StationaryStore> StationaryStores { get; set; } = default!;
 
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options)
-        {
-        } 
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options) : base (options) { }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            base.OnConfiguring(optionsBuilder);
+        protected override void OnConfiguring (DbContextOptionsBuilder optionsBuilder) {
+            base.OnConfiguring (optionsBuilder);
 
-            optionsBuilder.UseLazyLoadingProxies();
-        }  
+            optionsBuilder.UseLazyLoadingProxies ();
+        }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
+        protected override void OnModelCreating (ModelBuilder modelBuilder) {
+            base.OnModelCreating (modelBuilder);
 
-            modelBuilder.Entity<User>()
-                .ToTable("AspNetUsers")
-                .HasDiscriminator<int>("UserType")
-                .HasValue<User>((int)RoleValue.User)
-                .HasValue<Customer>((int)RoleValue.Customer)
-                .HasValue<Supplier>((int)RoleValue.Supplier)
-                .HasValue<StationaryStoreEmployee>((int)RoleValue.StationaryStoreEmployee)
-        } 
+            modelBuilder.Entity<User> ()
+                .ToTable ("AspNetUsers")
+                .HasDiscriminator<int> ("UserType")
+                .HasValue<User> (0)
+                .HasValue<Customer> (1)
+                .HasValue<Supplier> (2)
+                .HasValue<StationaryStoreEmployee> (3);
+        }
     }
 }

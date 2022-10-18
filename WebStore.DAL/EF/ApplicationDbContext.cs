d7e4a039e31a12dfeb37da6de25c4eeab901fafa
Adam_Dunaejwski <adam_dunajewski@doonaj.com>
2022-10-18 18:02:20 +0200

    cd

--- WebStore.DAL/EF/ApplicationDbContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-namespace WebStore.DAL.EF
-{
-    public class ApplicationDbContext : IdentitiyDbContext<User, Role, int>
-    {
-
-    }
-}
--- WebStore.Model/User.cs
+++ WebStore.Model/User.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 namespace WebStore.Model;
 public class User : IdentityUser<int>

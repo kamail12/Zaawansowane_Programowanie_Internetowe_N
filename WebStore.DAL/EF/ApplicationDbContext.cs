fdb485a11805678a0a70e4ef274e0255b1061a27
Adam Kozak <ak@mail.mail>
2022-10-21 00:40:14 +0200

    Lab1

--- WebStore.DAL/EF/ApplicationDbContext.cs
+++ WebStore.DAL/EF/ApplicationDbContext.cs
@@ -1,31 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using WebStore.Model.DataModels;
 
-namespace WebStore.DAL.EF
-{
-    public class ApplicationDbContext : Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityDbContext<User, IdentityRole<int>, int>
-    {
-        public virtual DbSet<OrderProduct> OrderProducts {get; set;} = default!;
-        public virtual DbSet<Product> Products {get; set;} = default!;
-        public virtual DbSet<ProductStock> ProductStocks {get; set;} = default!;
-        public virtual DbSet<Order> Orders {get; set;} = default!;
-        public virtual DbSet<Address> Addresses {get; set;} = default!;
-        public virtual DbSet<Category> Categories {get; set;} = default!;
-        public virtual DbSet<Invoice> Invoices {get; set;} = default!;
-        public virtual DbSet<StationaryStore> StationaryStores {get; set;} = default!;
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base (options){}
-        protected override void OnConfiguring (DbContextOptionsBuilder optionsBuilder)
-        {
+namespace WebStore.DAL.EF {
+    public class ApplicationDbContext : IdentityDbContext<User, IdentityRole<int>, int> {
+        public virtual DbSet<Order> Orders { get; set; } = default!;
+        public virtual DbSet<Address> Addresses { get; set; } = default!;
+        public virtual DbSet<Category> Categories { get; set; } = default!;
+        public virtual DbSet<Invoice> Invoices { get; set; } = default!;
+        public virtual DbSet<OrderProduct> OrderProducts { get; set; } = default!;
+        public virtual DbSet<Product> Products { get; set; } = default!;
+        public virtual DbSet<ProductStock> ProductStocks { get; set; } = default!;
+        
+        public virtual DbSet<StationaryStore> StationaryStores { get; set; } = default!;
+
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options) : base (options) { }
+
+        protected override void OnConfiguring (DbContextOptionsBuilder optionsBuilder) {
             base.OnConfiguring (optionsBuilder);
+
             optionsBuilder.UseLazyLoadingProxies ();
         }
-        protected override void OnModelCreating (ModelBuilder modelBuilder)
-        {
+
+        protected override void OnModelCreating (ModelBuilder modelBuilder) {
             base.OnModelCreating (modelBuilder);
 
             modelBuilder.Entity<User> ()
-            .ToTable ("AspNetUsers")
+                .ToTable ("AspNetUsers")
                 .HasDiscriminator<int> ("UserType")
                 .HasValue<User> (0)
                 .HasValue<Customer> (1)
@@ -45,6 +51,9 @@ namespace WebStore.DAL.EF
                 .WithMany(sg => sg.OrderProducts)
                 .HasForeignKey(g => g.OrderId)
                 .OnDelete(DeleteBehavior.Restrict);
+
+
+
         }
     }
 }

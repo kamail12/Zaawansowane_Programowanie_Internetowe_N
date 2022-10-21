963f25eb87ec8aa58f79bf18d2363e90b3df2e76
Adam Kozak <ak@mail.mail>
2022-10-21 20:49:59 +0200

    Lab1

--- WebStore.DAL/EF/ApplicationDbContext.cs
+++ WebStore.DAL/EF/ApplicationDbContext.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Design;
 using WebStore.Model.DataModels;
 
 namespace WebStore.DAL.EF {

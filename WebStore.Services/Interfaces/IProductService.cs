5b359cc411ac7218809306e4e2fbb53374cac9d4
MichalStuff <jakis@gmail.com>
2022-10-22 15:28:36 +0200

    test-install

---
+++ WebStore.Services/Interfaces/IProductService.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using WebStore.Model.DataModels;
+using WebStore.ViewModels.VM;
+namespace WebStore.Services.Interfaces
+{
+    public interface IProductService
+    {
+        ProductVm AddOrUpdateProduct(AddOrUpdateProductVm addOrUpdateProductVm);
+        ProductVm GetProduct(Expression<Func<Product, bool>> filterExpression);
+        IEnumerable<ProductVm> GetProducts(Expression<Func<Product, bool>>? filterExpression = null);
+    }
+}

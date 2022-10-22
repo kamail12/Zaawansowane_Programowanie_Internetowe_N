ad0620094ee682f51f1f08c5c50195e7f76be68a
Olesinski <mikolajolesinski1990@gmail.com>
2022-10-22 15:21:21 +0200

    2 laby czesc

--- WebStore.Web/Program.cs
+++ WebStore.Web/Program.cs
@@ -1,38 +1,42 @@
-global using WebStore.DAL.EF;
-global using Microsoft.EntityFrameworkCore;
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Identity;
+using WebStore.Services.Interfaces; 
+using Microsoft.IdentityModel.Tokens; 
+using System.Text; 
+using WebStore.ViewModels.VM;
+using Microsoft.OpenApi.Models;
+using WebStore.Model.DataModels; 
+using WebStore.Services.Configuration; 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using WebStore.Services.ConcreteServices;
+using WebStore.DAL.EF;
+using Microsoft.EntityFrameworkCore;
 
-// Add services to the container.
-
-
-// Add services to the container.
-
-// Add data context
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
+var builder = WebApplication.CreateBuilder(args); 
+// Add services to the container. 
+builder.Services.AddAutoMapper(typeof(MainProfile)); 
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")) //here you can define a database type. 
+); 
+builder.Services.Configure<JwtOptionsVm>(options =>builder.Configuration.GetSection("JwtOptions").Bind(options)); 
+builder.Services.AddIdentity<User, IdentityRole<int>>(o => 
+{ 
+    o.Password.RequireDigit = false; 
+    o.Password.RequireUppercase = false; 
+    o.Password.RequireLowercase = false; 
+    o.Password.RequireNonAlphanumeric = false; 
+    o.User.RequireUniqueEmail = false; 
+    }) .AddRoleManager<RoleManager<IdentityRole<int>>>() 
+        .AddUserManager<UserManager<User>>() 
+        .AddEntityFrameworkStores<ApplicationDbContext>() 
+        .AddDefaultTokenProviders(); 
+    builder.Services.AddAntiforgery(options => options.HeaderName = "X-XSRF-TOKEN"); 
+    builder.Services.AddTransient(typeof(ILogger), typeof(Logger<Program>)); 
+    builder.Services.AddScoped<IProductService, ProductService>(); 
+    builder.Services.AddScoped<IAddressService, AddressService>(); 
+    builder.Services.AddAuthentication(options => 
+            { 
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme; 
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme; 
+            }) 
+            .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseRouting();
-
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller}/{action=Index}/{id?}");
-
-app.MapFallbackToFile("index.html");;
-
-app.Run();

b9eba9d16a2db4d4061dc6f1c4dceb1f35e5c9c9
Olesinski <mikolajolesinski1990@gmail.com>
2022-10-22 13:23:52 +0200

    Działajacy test

--- WebStore.Services/Configuration/MainProfile.cs
+++ WebStore.Services/Configuration/MainProfile.cs
@@ -10,6 +10,8 @@ namespace WebStore.Services.Configuration{
         {
             CreateMap<AddOrUpdateProductVm, Product>();
             CreateMap<ProductVm, Product>();
+            CreateMap<Product, ProductVm>();
+        
             
         }
     }

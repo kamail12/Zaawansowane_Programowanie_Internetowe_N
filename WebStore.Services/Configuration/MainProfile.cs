0973b3d043ae26757239cbf641caf5c20204bdb7
BH <wymyslony@gmail.com>
2022-10-22 15:21:45 +0200

    test: add tests

---
+++ WebStore.Services/Configuration/MainProfile.cs
@@ -0,0 +1,19 @@
+using AutoMapper;
+using WebStore.Model.DataModels;
+using WebStore.ViewModels.VM;
+
+
+namespace WebStore.Services.Configuration
+{
+    public class MainProfile : Profile
+    {
+        public MainProfile()
+        {
+            CreateMap<AddOrUpdateProductVm, Product>();
+            CreateMap<ProductVm, Product>();
+            CreateMap<Product, ProductVm>();
+
+
+        }
+    }
+}

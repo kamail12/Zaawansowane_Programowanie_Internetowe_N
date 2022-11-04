9337664eb713bfefca7b4a9e6fc7cf73819e9d62
gkorzus <gkorzus@gmail.com>
2022-11-04 21:08:44 +0100

    Base correct

--- WebStore.Services/ConcreteServices/BaseService.cs
+++ WebStore.Services/ConcreteServices/BaseService.cs
@@ -1,16 +1,19 @@
-using AutoMapper; 
-using Microsoft.Extensions.Logging; 
-using WebStore.DAL.Data; 
-namespace WebStore.Services.ConcreteServices {
-     public abstract class BaseService {
-         protected readonly ApplicationDbContext DbContext; 
-         protected readonly ILogger Logger; 
-         protected readonly IMapper Mapper; 
-         public BaseService (ApplicationDbContext dbContext, 
-            IMapper mapper, ILogger logger) { 
-            DbContext = dbContext; 
-            Logger = logger; 
-            Mapper = mapper; 
-        } 
-    } 
+using AutoMapper;
+using Microsoft.Extensions.Logging;
+using WebStore.DAL.Data;
+namespace WebStore.Services.ConcreteServices
+{
+    public abstract class BaseService
+    {
+        protected readonly ApplicationDbContext DbContext;
+        protected readonly ILogger Logger;
+        protected readonly IMapper Mapper;
+        public BaseService(ApplicationDbContext dbContext,
+           IMapper mapper, ILogger logger)
+        {
+            DbContext = dbContext;
+            Logger = logger;
+            Mapper = mapper;
+        }
+    }
 }

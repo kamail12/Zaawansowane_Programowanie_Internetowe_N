10bd07c6e1e4ac86ad48fcece8e3e1027a313f3d
Adam Dunajewski <ad@doonaj.com>
2022-11-08 10:37:00 +0100

    rekonfiguracja

--- WebStore.Model/DataModels/CustomerAddress.cs
+++ WebStore.Model/DataModels/CustomerAddress.cs
@@ -4,8 +4,8 @@ namespace WebStore.Model.DataModels
 {
     public class CustomerAddress : Address
     {
-        public virtual Customer Customer { get; set; } = default!;
         [ForeignKey("CustomerAdresses")]
         public int CustomerId { get; set; }
+        public virtual Customer Customer { get; set; } = default!;
     }
 }

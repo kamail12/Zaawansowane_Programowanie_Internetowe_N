da169d824e3f35f2f1783df7be8a65d28268046c
Adam Dunajewski <ad@doonaj.com>
2022-11-11 22:53:00 +0100

    SoftDel and CleanUp

--- WebStore.DAL/DataAccess/ApplicationDbContext.cs
+++ WebStore.DAL/DataAccess/ApplicationDbContext.cs
@@ -9,14 +9,14 @@ public class ApplicationDbContext : IdentityDbContext<User, IdentityRole<int>, i
 {
 
     public virtual DbSet<User> User { get; set; } = default!;
-    public virtual DbSet<CustomerAddress> CustomerAddresses { get; set; } = default!;
+    public virtual DbSet<Address> Address { get; set; } = default!;
     public virtual DbSet<OrderProduct> OrderProduct { get; set; } = default!;
     public virtual DbSet<Product> Product { get; set; } = default!;
     public virtual DbSet<ProductStock> ProductStock { get; set; } = default!;
     public virtual DbSet<Order> Order { get; set; } = default!;
     public virtual DbSet<Category> Category { get; set; } = default!;
     public virtual DbSet<Invoice> Invoice { get; set; } = default!;
-    public virtual DbSet<StationaryStore> StationaryStores { get; set; } = default!;
+    public virtual DbSet<StationaryStore> StationaryStore { get; set; } = default!;
 
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
@@ -27,6 +27,17 @@ public class ApplicationDbContext : IdentityDbContext<User, IdentityRole<int>, i
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
+
+        builder.Entity<Address>().Property<bool>("IsDeleted");
+        builder.Entity<Address>().HasQueryFilter(m => EF.Property<bool>(m, "IsDeleted") == false);
+        builder.Entity<Invoice>().Property<bool>("IsDeleted");
+        builder.Entity<Invoice>().HasQueryFilter(m => EF.Property<bool>(m, "IsDeleted") == false);
+        builder.Entity<Order>().Property<bool>("IsDeleted");
+        builder.Entity<Order>().HasQueryFilter(m => EF.Property<bool>(m, "IsDeleted") == false);
+        builder.Entity<Product>().Property<bool>("IsDeleted");
+        builder.Entity<Product>().HasQueryFilter(m => EF.Property<bool>(m, "IsDeleted") == false);
+        builder.Entity<StationaryStore>().Property<bool>("IsDeleted");
+        builder.Entity<StationaryStore>().HasQueryFilter(m => EF.Property<bool>(m, "IsDeleted") == false);
 
         builder.Entity<User>()
             .ToTable("AspNetUsers")
@@ -49,5 +60,33 @@ public class ApplicationDbContext : IdentityDbContext<User, IdentityRole<int>, i
             .WithMany(y => y.OrderProducts)
             .HasForeignKey(x => x.ProductId)
             .OnDelete(DeleteBehavior.Cascade);
+    }
+    public override int SaveChanges()
+    {
+        UpdateSoftDeleteStatuses();
+        return base.SaveChanges();
+    }
+
+    public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default(CancellationToken))
+    {
+        UpdateSoftDeleteStatuses();
+        return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+    }
+
+    private void UpdateSoftDeleteStatuses()
+    {
+        foreach (var entry in ChangeTracker.Entries())
+        {
+            switch (entry.State)
+            {
+                case EntityState.Added:
+                    entry.CurrentValues["IsDeleted"] = false;
+                    break;
+                case EntityState.Deleted:
+                    entry.State = EntityState.Modified;
+                    entry.CurrentValues["IsDeleted"] = true;
+                    break;
+            }
+        }
     }
 }

a007272b71dada3bf212b8e49616bc1c723c3ff7
MichalStuff <jakis@gmail.com>
2022-11-13 18:30:59 +0100

    Proxy and Store error repair

--- WebStore.Services/ConcreteServices/StoreService.cs
+++ WebStore.Services/ConcreteServices/StoreService.cs
@@ -15,20 +15,21 @@ namespace WebStore.Services.ConcreteServices
     {
         public StoreService(ApplicationDbContext dbContext, IMapper mapper, ILogger logger) : base(dbContext, mapper, logger) { }
 
-        public StoreVm GetStore(Expression<Func<StationaryStore, bool>> filterExpression)
-        {
-            try
-            {
-                if (filterExpression == null)
-                    throw new ArgumentNullException("Filter expression parameter is null");
-                // var storeEntity = DbContext.
-            }
-            catch (Exception ex)
-            {
-                Logger.LogError(ex, ex.Message);
-                throw;
-            }
-        }
+        // public StoreVm GetStore(Expression<Func<StationaryStore, bool>> filterExpression)
+        // {
+        //     try
+        //     {
+        //         if (filterExpression == null)
+        //             throw new ArgumentNullException("Filter expression parameter is null");
+
+        //          var storeEntity = DbContext.St
+        //     }
+        //     catch (Exception ex)
+        //     {
+        //         Logger.LogError(ex, ex.Message);
+        //         throw;
+        //     }
+        // }
 
     }
 }
--- WebStore.Web/ClientApp/src/setupProxy.js
+++ WebStore.Web/ClientApp/src/setupProxy.js
@@ -1,14 +1,17 @@
 const createProxyMiddleware = require('http-proxy-middleware');
-const { env } = require('process');
+const {
+  env
+} = require('process');
 
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
   env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:13191';
 
-const context =  [
-  "/weatherforecast",
+const context = [
+  "/api",
+  "/swagger"
 ];
 
-module.exports = function(app) {
+module.exports = function (app) {
   const appProxy = createProxyMiddleware(context, {
     target: target,
     secure: false,

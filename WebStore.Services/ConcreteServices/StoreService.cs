99866c3c07e35dded8aedb63fd3f5d75756c2b4d
MichalStuff <jakis@gmail.com>
2022-11-17 14:34:47 +0100

    lab2 : error

--- WebStore.Services/ConcreteServices/StoreService.cs
+++ WebStore.Services/ConcreteServices/StoreService.cs
@@ -1,34 +1,34 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using AutoMapper;
-using Microsoft.Extensions.Logging;
-using WebStore.DAL.EF;
-using WebStore.Model.DataModels;
-using WebStore.Services.Interfaces;
-using WebStore.ViewModels.VM;
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Linq.Expressions;
+// using AutoMapper;
+// using Microsoft.Extensions.Logging;
+// using WebStore.DAL.EF;
+// using WebStore.Model.DataModels;
+// using WebStore.Services.Interfaces;
+// using WebStore.ViewModels.VM;
 
-namespace WebStore.Services.ConcreteServices
-{
-    public class StoreService : BaseService, IStoreService
-    {
-        public StoreService(ApplicationDbContext dbContext, IMapper mapper, ILogger logger) : base(dbContext, mapper, logger) { }
+// namespace WebStore.Services.ConcreteServices
+// {
+//     public class StoreService : BaseService, IStoreService
+//     {
+//         public StoreService(ApplicationDbContext dbContext, IMapper mapper, ILogger logger) : base(dbContext, mapper, logger) { }
 
-        public StoreVm GetStore(Expression<Func<StationaryStore, bool>> filterExpression)
-        {
-            try
-            {
-                if (filterExpression == null)
-                    throw new ArgumentNullException("Filter expression parameter is null");
-                // var storeEntity = DbContext.
-            }
-            catch (Exception ex)
-            {
-                Logger.LogError(ex, ex.Message);
-                throw;
-            }
-        }
+//         public StoreVm GetStore(Expression<Func<StationaryStore, bool>> filterExpression)
+//         {
+//             try
+//             {
+//                 if (filterExpression == null)
+//                     throw new ArgumentNullException("Filter expression parameter is null");
+//                 // var storeEntity = DbContext.
+//             }
+//             catch (Exception ex)
+//             {
+//                 Logger.LogError(ex, ex.Message);
+//                 throw;
+//             }
+//         }
 
 //     }
 // }
